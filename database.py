a3cd0cfebb1cd1e431338ace489c44018a0a15f6
AlexSitohov <lilinfinitemob@gmail.com>
2022-12-13 17:32:15 +0300

    deploy

--- database.py
+++ database.py
@@ -2,7 +2,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:123@localhost/fastapi_shop"
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres:wGwxA301mTBxnPaaz5Dz@containers-us-west-162.railway.app:7030/railway"
+
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL,
     # echo=True
